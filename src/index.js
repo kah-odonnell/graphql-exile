2c4efa3323bb718863992d94e3ad825d333663fe
Kyle <kah.odonnell@gmail.com>
2019-10-01 08:57:55 -0400

    updated CORS

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import uuidv4 from 'uuid/v4';
 import jwt from 'jsonwebtoken';
 import express from 'express';
 import {
-  ApolloServer,
-  AuthenticationError,
+    ApolloServer,
+    AuthenticationError,
 } from 'apollo-server-express';
 
 import schema from './schema';
@@ -14,46 +14,48 @@ import models, { sequelize } from './models';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://battle-mages.herokuapp.com'
+}));
 
 const getMe = async req => {
-  const token = req.headers['x-token'];
+    const token = req.headers['x-token'];
 
-  if (token) {
-    try {
-      return await jwt.verify(token, process.env.SECRET);
-    } catch (e) {
-      throw new AuthenticationError(
-        'Your session expired. Sign in again.',
-      );
+    if (token) {
+        try {
+            return await jwt.verify(token, process.env.SECRET);
+        } catch (e) {
+            throw new AuthenticationError(
+                'Your session expired. Sign in again.',
+            );
+        }
     }
-  }
 };
 
 const server = new ApolloServer({
-  typeDefs: schema,
-  resolvers,
-  formatError: error => {
-    // remove the internal sequelize error message
-    // leave only the important validation error
-    const message = error.message
-      .replace('SequelizeValidationError: ', '')
-      .replace('Validation error: ', '');
+    typeDefs: schema,
+    resolvers,
+    formatError: error => {
+        // remove the internal sequelize error message
+        // leave only the important validation error
+        const message = error.message
+            .replace('SequelizeValidationError: ', '')
+            .replace('Validation error: ', '');
 
-    return {
-      ...error,
-      message,
-    };
-  },
-  context: async ({ req }) => {
-    const me = await getMe(req);
+        return {
+            ...error,
+            message,
+        };
+    },
+    context: async ({ req }) => {
+        const me = await getMe(req);
 
-    return {
-      models,
-      me,
-      secret: process.env.SECRET,
-    };
-  },
+        return {
+            models,
+            me,
+            secret: process.env.SECRET,
+        };
+    },
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
@@ -61,52 +63,52 @@ server.applyMiddleware({ app, path: '/graphql' });
 const eraseDatabaseOnSync = true;
 
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
-  if (eraseDatabaseOnSync) {
-    createUsersWithMessages(new Date());
-  }
+    if (eraseDatabaseOnSync) {
+        createUsersWithMessages(new Date());
+    }
 
-  app.listen({ port: 8000 }, () => {
-    console.log('Apollo Server on http://localhost:8000/graphql');
-  });
+    app.listen({ port: 8000 }, () => {
+        console.log('Apollo Server on http://localhost:8000/graphql');
+    });
 });
 
 const createUsersWithMessages = async date => {
-  await models.User.create(
-    {
-      username: 'rwieruch',
-      email: 'hello@robin.com',
-      password: 'rwieruch',
-      role: 'ADMIN',
-      messages: [
+    await models.User.create(
         {
-          text: 'Published the Road to learn React',
-          createdAt: date.setSeconds(date.getSeconds() + 1),
+            username: 'rwieruch',
+            email: 'hello@robin.com',
+            password: 'rwieruch',
+            role: 'ADMIN',
+            messages: [
+                {
+                    text: 'Published the Road to learn React',
+                    createdAt: date.setSeconds(date.getSeconds() + 1),
+                },
+            ],
         },
-      ],
-    },
-    {
-      include: [models.Message],
-    },
-  );
+        {
+            include: [models.Message],
+        },
+    );
 
-  await models.User.create(
-    {
-      username: 'ddavids',
-      email: 'hello@david.com',
-      password: 'ddavids',
-      messages: [
+    await models.User.create(
         {
-          text: 'Happy to release ...',
-          createdAt: date.setSeconds(date.getSeconds() + 1),
+            username: 'ddavids',
+            email: 'hello@david.com',
+            password: 'ddavids',
+            messages: [
+                {
+                    text: 'Happy to release ...',
+                    createdAt: date.setSeconds(date.getSeconds() + 1),
+                },
+                {
+                    text: 'Published a complete ...',
+                    createdAt: date.setSeconds(date.getSeconds() + 1),
+                },
+            ],
         },
         {
-          text: 'Published a complete ...',
-          createdAt: date.setSeconds(date.getSeconds() + 1),
+            include: [models.Message],
         },
-      ],
-    },
-    {
-      include: [models.Message],
-    },
-  );
+    );
 };
